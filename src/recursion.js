2eb1cb2f60b932ddd3402bdb024485bda4932c42
stephenJohnGiardina <stephengia@yahoo.com>
2020-02-25 16:03:43 -0500

    Completion of exponent and powerOfTwo

--- src/recursion.js
+++ src/recursion.js
@@ -73,6 +73,17 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+	if (exp === 0) {
+		return 1;
+	}
+	if (exp > 0) {
+		if (exp === 1) {
+			return base;
+		}
+		return base * exponent(base, exp - 1);
+	} else {
+		return 1 / exponent(base, - exp);
+	}
 };
 
 // 8. Determine if a number is a power of two.
@@ -80,6 +91,15 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+	if (n === 2 || n === 1) {
+		return true;
+	}
+	if (n < 2) {
+		return false;
+	}
+	if (n > 2) {
+		return powerOfTwo(n / 2);
+	}
 };
 
 // 9. Write a function that reverses a string.
