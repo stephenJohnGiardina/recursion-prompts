2115219758c53b3a7b7b3da5455631919c2bb779
stephenJohnGiardina <stephengia@yahoo.com>
2020-02-26 18:46:07 -0500

    Completion of flatten

--- src/recursion.js
+++ src/recursion.js
@@ -480,6 +480,18 @@ var nestedEvenSum = function(obj) {
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
 var flatten = function(array) {
+	let list = [];
+	if (array.length === 0) {
+		return [];
+	}
+	for (let i = 0; i < array.length; i++) {
+		if (Array.isArray(array[i])) {
+			list = list.concat(flatten(array[i]));
+		} else {
+			list.push(array[i]);
+		}
+	}
+	return list;
 };
 
 // 31. Given a string, return an object containing tallies of each letter.
