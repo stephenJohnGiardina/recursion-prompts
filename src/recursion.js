2285581e3c86955b69656e5427af8b0f211075c0
stephenJohnGiardina <stephengia@yahoo.com>
2020-03-09 12:06:02 -0400

    Completion of fibonacci and nthFibo

--- src/recursion.js
+++ src/recursion.js
@@ -406,6 +406,15 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+	if (n <= 0) {
+		return null;
+	} else if (n === 1) {
+		return [0, 1];
+	} else {
+		arr = fibonacci(n - 1);
+		arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+		return arr;
+	}
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
@@ -414,6 +423,10 @@ var fibonacci = function(n) {
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
+	if (n < 0) { return null; }
+	else if (n === 0) { return 0; }
+	else if (n === 1) { return 1; } 
+	else { return nthFibo(n - 1) + nthFibo(n - 2); }
 };
 
 // 27. Given an array of words, return a new array containing each word capitalized.
